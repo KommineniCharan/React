84bef3ada4d68e57c5247de33e226407abbff561
KommineniCharan <charank347@gmail.com>
2023-11-24 14:46:55 +0530

    dbchangeall

--- GrankWebsiteServer/v1/models/addtocart.model.js
+++ GrankWebsiteServer/v1/models/addtocart.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 const AutoIncrement = require("mongoose-sequence")(mongoose);
 
 const addtocartSchema = new mongoose.Schema({
